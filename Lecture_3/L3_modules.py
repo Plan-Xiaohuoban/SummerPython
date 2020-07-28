ec57d238a859373f85bab276202cfbbb1a2ec2a8
Arcadia <29194544+Arcadia-1@users.noreply.github.com>
2020-07-28 21:55:42 +0800

    deleted a zip

--- Lecture_3/L3_modules.py
+++ Lecture_3/L3_modules.py
@@ -1,18 +1,17 @@
 
 # 三种常见的 import 方法
 import L3_inherit  # 直接导入
-import cats.L3_pillow_filters
+
+import cats.L3_pillow_filters as cf
+
+import sys
+
+sys.path.append("../")
+import Lecture_2.L2_list_comprehension as L2
+
+print(L2.scores)
 # import L3_inherit as cc  # 导入并且起一个别名
 # from L3_inherit import Hanxin,Libai # 选择性导入一部分
 
 # # 非常不建议的写法
-from L3_inherit import *  # 导入模块中的全部
 
-a= Hanxin("韩信", 15)
-a.StealTower()
-
-# a = Hanxin("韩信", 15)
-# a.StealTower()
-
-b = Libai("Libai")
-b.use_skill(2)
